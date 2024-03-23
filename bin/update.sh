fa072fcef687473209b9fd1baf7ea07487c1a6f0
Saori Isikawa <saori.isikawa414@gmail.com>
2024-03-23 17:54:53 +0800

    更新

---
+++ bin/update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#sudo ntpd -qg && sudo hwclock -w -v
+sudo apt update && sudo apt upgrade
+sudo snap refresh
+#fc-cache -fv
+#sudo fc-cache -fv
+#sudo update-initramfs -c -k "$(uname -r)"
+#sudo update-grub
