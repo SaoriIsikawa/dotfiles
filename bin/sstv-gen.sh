d3e52220669ac2b1f069bd6966da0b77398da87c
Saori Isikawa <saori.isikawa414@gmail.com>
2024-05-12 00:54:06 +0800

    更新

--- bin/sstv-gen.sh
+++ bin/sstv-gen.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-cp -f ./backup/*.png .
+cp -f ./backup/*640x496*.png .
 for filename in *.png
-  do convert -fill white -background '#00000080' \
-  -gravity North -size 640x80 \
-  -font "Noto-Sans-Mono-SemiBold" \
+  do convert -fill yellow -background '#00000000' \
+  -gravity North -size 640x100  \
+  -font "Cascadia-Mono-SemiBold" \
   caption:'CQ SSTV    de BG7XTQ' $filename \
   +swap -gravity north -composite $filename
 done
 # -font "Noto-Sans-Mono-SemiBold" \
 # -font "Source-Code-Pro-Semibold" \
+# -font "Cascadia-Mono-SemiBold" \
 
 rm -f *.wav
 for filename in *.png
-   do pisstv -r 22050 -p pd120 $filename
+  do pisstv -r 44100 -p pd120 $filename
 done
+# do pisstv -r 44100 -p pd120 $filename
+# do /usr/local/bin/sstv-encode pd290 $filename $filename.wav 44100
 
-sox -n -r 22050 -c 1 -b 16 /tmp/silence-5s.wav trim 0.0 5
+sox -n -r 44100 -c 1 /tmp/silence-5s.wav trim 0.0 5
 sox $(for f in *.wav; do echo -n "$f /tmp/silence-5s.wav "; done) output.wav
 
